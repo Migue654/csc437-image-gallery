18cef51454fbb208e875c727889a8c93b04b4ce6
Migue654 <mmedin52@calpoly.edu>
2026-03-05 14:25:27 -0800

    Lab 19:Deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
+// App.jsx
+import { Routes, Route } from "react-router-dom";
 import { AllImages } from "./images/AllImages.jsx";
 import { ImageDetails } from "./images/ImageDetails.jsx";
 import { UploadPage } from "./UploadPage.jsx";
 import { LoginPage } from "./LoginPage.jsx";
-
+import { MainLayout } from "./MainLayout.jsx";
+import { Valid_Routes } from "./shared/ValidRoutes.js";
 function App() {
-    const POSSIBLE_PAGES = [
-        <AllImages />,
-        <ImageDetails imageId={"0"} />,
-        <UploadPage />,
-        <LoginPage />
-    ];
-
-    return POSSIBLE_PAGES[0];
+  return (
+    <Routes>
+    <Route element={<MainLayout/>}>
+      <Route path={Valid_Routes.HOME} element={<AllImages />} />
+      <Route path="/images/:imageId" element={<ImageDetails />} />
+      <Route path={Valid_Routes.UPLOAD} element={<UploadPage />} />
+      <Route path={Valid_Routes.LOGIN} element={<LoginPage />} />
+    </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/MainLayout.jsx
+++ frontend/src/MainLayout.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Header } from "./Header.jsx";
+import { Outlet } from "react-router"
 
-export function MainLayout(props) {
+export function MainLayout() {
     return (
+
         <div>
             <Header />
             <div style={{padding: "0 2em"}}>
-                {props.children}
+                <Outlet />
             </div>
         </div>
+
     );
 }
+// {props.children}
